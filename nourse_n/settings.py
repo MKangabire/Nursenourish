806c6eff1e0327bd04c299ef57f4775ce07d9ff0
MKangabire <m.kangabire@alustudent.com>
2024-08-05 11:43:41 +0200

    settings changes

--- nourse_n/settings.py
+++ nourse_n/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-#xjb$az)(3rt4ynbs)i-#tc=^-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
 
-ALLOWED_HOSTS = ['nursenourish.me', 'www.nursenourish.me', 'nursenourish1.com', 'www.nursenourish1.com']
+ALLOWED_HOSTS = ['nursenourish.vercel.app', 'nursenourish1.com', 'www.nursenourish1.com', '127.0.0.1']
 
 
 # Application definition
